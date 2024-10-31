5a3e8fa16c1ce911252575a0276647b4105e1dc7
Колобанов <zahark456@gmail.com>
2024-10-31 08:33:58 +0300

    Проба пера

---
+++ LR2/Vid_odezhdi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2
+{
+    public enum Vidi_Odezhdi
+    {
+        Zadidas = 0,
+        Zike,
+        Zoika_gobana,
+        Zuma,
+        Zalenciaga,
+        Ztone_Zisland,
+    }
+}
